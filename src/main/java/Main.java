3674f9dffa9fe8d49e2835656c5d202c638e61a9
Bryan Macedo <contato.bryansm@hotmail.com>
2025-05-24 10:05:03 -0300

    Comecei a imprimir os dois eventos de test da função.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,18 +4,10 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        // fazer uma função q receba n registros
-        // q representem o consumo de bateria e eventos de carregamento
-        // um valor POSITIVO indica MINUTOS CARREGADOS
-        // um valor NEGATIVO indica MINUTOS JOGANDO(o laptop consome 1% de bateria por minuto)
-        // a bateria não pode passar de 100%
-        // a carga inicial é de 50%
-        // -> a função deve RETORNAR A PORCENTAGEM FINAL DA BATERIA
-        // EX: N = 4 EVENTOS[10, -20, 61, -15] SAIDA = 85
-
         List<Integer> events = new ArrayList<>(Arrays.asList(10, -20, 61, -15));
         List<Integer> events02 = new ArrayList<>(Arrays.asList(10, -20, -40, -15));
 
+        System.out.println("Final: " + getBattery(events));
         System.out.println("Final: " + getBattery(events02));
     }
 
